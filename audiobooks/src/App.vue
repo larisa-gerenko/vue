ae1b07dc8787a0130b15d055e0bd2a6f7d48eb59
Larisa <larisavalgutova@gmail.com>
2020-07-20 23:24:55 +0300

    1

--- audiobooks/src/App.vue
+++ audiobooks/src/App.vue
@@ -3,11 +3,11 @@
     <div class="box-center">
       <audio-player
         :audiotracks="[
-          {name: 'Summer', src: 'https://www.bensound.com/bensound-music/bensound-summer.mp3'},
-        {name: 'Ukulele', src: 'https://www.bensound.com/bensound-music/bensound-ukulele.mp3'},
-        {name: 'Creative Minds', src: 'https://www.bensound.com/bensound-music/bensound-creativeminds.mp3'},
-        {name: 'A New Beginning', src: 'https://www.bensound.com/bensound-music/bensound-anewbeginning.mp3'},
-        {name: 'Little Idea', src: 'https://www.bensound.com/bensound-music/bensound-littleidea.mp3'}]"
+          {name: 'Summer', src: 'https://www.bensound.com/bensound-music/bensound-summer.mp3', time: '2:43'},
+        {name: 'Ukulele', src: 'https://www.bensound.com/bensound-music/bensound-ukulele.mp3', time: '2:43'},
+        {name: 'Creative Minds', src: 'https://www.bensound.com/bensound-music/bensound-creativeminds.mp3', time: '2:43'},
+        {name: 'A New Beginning', src: 'https://www.bensound.com/bensound-music/bensound-anewbeginning.mp3', time: '2:43'},
+        {name: 'Little Idea', src: 'https://www.bensound.com/bensound-music/bensound-littleidea.mp3', time: '2:43'}]"
       ></audio-player>
     </div>
   </div>
@@ -26,6 +26,7 @@ export default {
 
 <style>
 @import url("https://use.fontawesome.com/releases/v5.0.13/css/all.css");
+@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;0,500;1,400&display=swap");
 
 #app {
   display: flex;
@@ -34,9 +35,8 @@ export default {
   height: 100vh;
 }
 .box-center {
-  width: 400px;
+  width: 500px;
   box-shadow: 0 0 45px -8px #c1c1c1;
-  padding: 10px;
   border-radius: 5px;
   border: 1px solid #efefef;
 }
--- audiobooks/src/components/AudioPlayer.vue
+++ audiobooks/src/components/AudioPlayer.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <div class="image"></div>
+  <div class="audioplayer">
+    <div class="image">
+      <img src="@/assets/logo.jpg" alt />
+    </div>
     <div class="player-wrapper">
       <div class="player">
         <div class="player-controls">
@@ -20,14 +22,22 @@
             <i class="fa fa-step-forward" aria-hidden="true"></i>
           </button>
         </div>
+        <div class="volume">
+          <input type="range" v-on:change="changeVolume" min="0" max="1" value="1" step="0.1" />
+        </div>
       </div>
     </div>
+
     <div class="list">
       <div
+        class="track"
         v-for="(track, index) in audiotracks"
         v-bind:key="track.name"
         v-on:click="playTrack(track.src, index)"
-      >{{track.name}}</div>
+      >
+        <span>{{track.name}}</span>
+        <span>{{track.time}}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -43,6 +53,13 @@ export default {
     };
   },
   props: ["audiotracks"],
+  mounted() {
+    const track = this.audiotracks[this.currentIndex];
+    this.currentTrack.src = track.src;
+    this.currentTrack.onended = () => {
+      this.nextTrack();
+    };
+  },
   methods: {
     play() {
       this.currentTrack.play();
@@ -52,10 +69,13 @@ export default {
       this.currentTrack.currentTime = 0;
     },
     playTrack(src, index) {
-      this.currentTrack.currentTime = 0;
       this.currentTrack.src = src;
       this.currentTrack.play();
       this.currentIndex = index;
+
+      this.currentTrack.onended = () => {
+        this.nextTrack();
+      };
     },
     nextTrack() {
       const nextIndex = this.currentIndex + 1;
@@ -72,30 +92,41 @@ export default {
       }
       const prevTrack = this.audiotracks[prevIndex];
       this.playTrack(prevTrack.src, prevIndex);
+    },
+    changeVolume(ev) {
+      this.currentTrack.volume = ev.target.value;
     }
   }
 };
 </script>
 
 <style scoped>
-/* .image {
-    background-image: url(./src/assets/logo.jpg);
- }
- */
-
+.audioplayer {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.image {
+  height: 200px;
+}
 .player-wrapper {
   align-items: center;
   background-color: #fff;
   display: flex;
+  flex-grow: 1;
   justify-content: center;
 }
-
+.volume {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
 .player-controls {
   display: flex;
 }
 
 button {
-  margin-right: 30px;
+  margin: 0 15px;
   color: black;
   background: none;
   border: none;
@@ -105,5 +136,22 @@ button {
 .play {
   color: #6e6 !important;
   font-size: 30px;
+}
+.list {
+  box-shadow: inset 0 6px 16px -12px #c1c1c1;
+  height: auto;
+  width: 500px;
+  border: 1px solid #eee;
+}
+.track {
+  display: flex;
+  justify-content: space-between;
+  margin-left: 5px;
+  color: #666;
+  font-family: "Roboto", sans-serif;
+  font-size: 16px;
+  padding: 5px;
+  border-bottom: 1px solid #eee;
+  line-height: 2;
 }
 </style>
